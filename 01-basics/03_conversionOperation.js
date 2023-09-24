1d85ca999f1bb1720ce7de0c77b39e2f4b0b9518
Moumin Ahmad <116239910+Moumin-pk@users.noreply.github.com>
2023-09-24 08:12:00 +0500

    conversion or  operater

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -31,4 +31,43 @@ console.log("num => "+typeof(num));
 let convertstring = String(num);
 console.log('convertstring => '+typeof(convertstring));
 
+/**** Operater ****/
+
+let value = 5;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2+2); // addidtion
+console.log(2-2); // substraction
+console.log(2*2); // multiplication
+console.log(2**3); // power
+console.log(2/3); // addidtion
+console.log(2%3); // reminder
+
+
+let fName = 'Moumin';
+let lName = 'Ahmad';
+let firstLastName =fName + lName;
+console.log(firstLastName);
+
+console.log('1'+2);  // 12
+console.log('1'+2);  // 21
+console.log("1"+2+2); // 122
+console.log(1+2+'2'); //32
+console.log(+true); // 1
+console.log(+""); // 0
+
+//practice  =>
+// increment(++)
+
+// postfix increment
+let x = 2;
+let y = x++
+console.log('postfix => '+ 'x = '+x+' y = '+y);
+
+// prefix increment
+
+let a =2;
+let b = ++a;
+console.log('postfix => '+ 'a = '+a+' b = '+b);
 
