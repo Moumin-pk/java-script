b79f647e222245d6bd2d7aab0978f804e094c98f
Moumin Ahmad <116239910+Moumin-pk@users.noreply.github.com>
2023-10-03 11:58:48 +0500

    IIFE-Concept

---
+++ 03_basics/04_life.js
@@ -0,0 +1,23 @@
+
+// Imediately invoked Function Expressions (IIFE)
+
+//simple function
+function one(){
+console.log('Hello-Friends👋');
+}
+one();
+
+// imedaitly invoke function
+
+// named IIFE
+(function one(){
+    console.log('Hello-Friends👋');
+    })();  // must use semicolen ;
+
+// arrow function 
+// unnamed IIFE
+(() => {
+    console.log('hellio-Moumin❌');
+})()
+
+//
