cdeb147fdc132c9cbe5a1ed344e00db94e5ab889
Moumin Ahmad <116239910+Moumin-pk@users.noreply.github.com>
2023-10-03 15:32:18 +0500

    execution context

---
+++ 03_basics/05_execution context.js
@@ -0,0 +1,25 @@
+let val1 =10;
+let val2 = 5;
+function addNum(num1, num2){
+    let total = num1 + num2
+    return total
+}
+let result1 = addNum(val1, val2)
+let result2 =addNum(10,2)
+console.log(result1);
+console.log(result2);
+
+
+// -> Global exection | -> this
+
+// Memory Phase 👋
+
+//val1 -> undefined
+//val2 -> undefined
+// addnum -> defination
+// result1 -> undefined
+// result2 -> undefined
+
+// exectution Phase
+
+                      
