16a116aca874d51570d878afb517fa772ad463ef
Moumin Ahmad <116239910+Moumin-pk@users.noreply.github.com>
2023-10-05 09:47:52 +0500

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,18 @@
+// switch => use for multiple condition
+
+let key = 2;
+switch (key) {
+    case 1:
+        console.log(`january`);
+        break;
+    case 2:
+        console.log(`Feburary`);
+        break;
+    case 3:
+        console.log(`March`);
+        break;
+
+    default:
+        console.log(`condition is not enougth`);
+        break;
+}
